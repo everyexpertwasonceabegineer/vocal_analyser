7d594077bfffe7d83911d8f2475b8ab9de60b158
merrickho <51411999+merrickho@users.noreply.github.com>
2019-06-11 21:44:16 +0800

    Add files via upload
    setTimeout on the front end that sends a POST request to the backend with the recorded audio (in 2s chunks), get the emotions. Then merge all the 2s chunks together, annotate the regions and plot a playback

--- src/RecordInterface.js
+++ src/RecordInterface.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import {getAudioStream, exportBuffer} from './utilities/audio.js';
+import {getAudioStream, exportBuffer, mergeBuffers} from './utilities/audio.js';
 import Recorder from 'recorder-js';
 import ResultsContainer from './ResultsContainer';
 import './ResultsContainer.css';
 import Waveform from './Waveform'
 
 const axios = require('axios');
-
 
 
 class RecordInterface extends React.Component {
@@ -17,6 +16,8 @@ class RecordInterface extends React.Component {
             recording: false,
             recorder: null,
             emotions: null,
+            audio: null,
+            bufferstack: []
         };
     }
     async componentDidMount() {
@@ -40,11 +41,22 @@ class RecordInterface extends React.Component {
         this.setState(
             {
                 recorder,
-                recording: true
+                recording: true,
+                audio: null
             },
             () => {
                 recorder.start();
             }
+        );
+
+        // testing out multiple calls to API
+        setTimeout(
+            () => this.intermediateStop(),
+            3000
+        );
+        setTimeout(
+            () => this.stopRecord(),
+            6000
         );
     }
 
@@ -54,27 +66,46 @@ class RecordInterface extends React.Component {
         const audio = exportBuffer(buffer[0]);
 
         //Do your audio processing here.
-        const formData = new FormData();
-        formData.append("wave", audio)
-        axios({
-            url: 'http://127.0.0.1:5000/vocal_analyser/api/analyse',
-            method: 'POST',
-            data: formData,
-            headers: {
-              Accept: 'application/json',
-              'Content-Type': 'multipart/form-data'
-            }
-        })
-        .then(res => {
-            console.log(res)
-            this.setState({emotions: res["data"]})
-            console.log(this.state.emotions)
-        })
+        this.calltoAPI(audio);
+
+        // add last part of audio
+        this.state.bufferstack.push(buffer[0])
 
         this.setState({
             recording: false,
+            audio: exportBuffer(mergeBuffers(this.state.bufferstack, this.state.bufferstack.length * this.state.bufferstack[0].length))
         });
     }
+
+    async intermediateStop() {
+        const { recorder } = this.state;
+        const { buffer } = await recorder.stop();
+        const audio = exportBuffer(buffer[0]);
+        this.state.bufferstack.push(buffer[0])
+
+        //Do your audio processing here.
+        this.calltoAPI(audio);
+        this.state.recorder.start();
+    }
+
+    async calltoAPI(audio){
+      const formData = new FormData();
+      formData.append("wave", audio)
+      axios({
+          url: 'http://127.0.0.1:5000/vocal_analyser/api/analyse',
+          method: 'POST',
+          data: formData,
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'multipart/form-data'
+          }
+      })
+      .then(res => {
+          this.setState({emotions: res["data"]})
+          console.log(this.state.emotions)
+      })
+    }
+
 
     render() {
         const { recording, stream } = this.state;
@@ -84,7 +115,7 @@ class RecordInterface extends React.Component {
         }
         return (
             <div>
-                <Waveform/>
+                <Waveform audio = {this.state.audio} />
                 {this.state.emotions == null? <div className = "ResultsContainer">No audio uploaded</div> : <ResultsContainer data= {this.state.emotions}/>}
                 <button
                     onClick={() => {
--- src/Waveform.js
+++ src/Waveform.js
@@ -1,37 +1,44 @@
 import React from 'react';
 import WaveSurfer from 'wavesurfer.js';
-import Regions from 'wavesurfer.js/src/plugin/regions.js';
+import RegionsPlugin from 'wavesurfer.js/src/plugin/regions.js';
 import TimelinePlugin from 'wavesurfer.js/src/plugin/timeline.js';
-//import Cursor from 'wavesurfer.js/src/plugin/cursor.js'
+import "./Waveform.css"
 
 
 export default class Waveform extends React.Component{
+
 
   constructor(props) {
       super(props);
       this.state = {
           wavesurfer: null,
+          playing: false,
+          regionnote: "-"
       };
   }
 
   async componentDidMount(){
+    // initialise wavesurfer instance
     var wavesurfer = WaveSurfer.create({
       container: '#waveform',
       waveColor: 'violet',
       progressColor: 'purple',
       scrollParent: true,
+      minimap: true,
       plugins: [
-        Regions.create(
+        RegionsPlugin.create(
           {
               regions: [
                   {
                       start: 1,
                       end: 3,
-                      color: 'hsla(400, 100%, 30%, 0.5)'
+                      color: 'hsla(400, 100%, 30%, 0.5)',
+                      "data": { "note": "羅生門" }
                   }, {
                       start: 5,
                       end: 7,
-                      color: 'hsla(200, 50%, 70%, 0.4)'
+                      color: 'hsla(200, 50%, 70%, 0.4)',
+                      "data": { "note": "芥川龍之介" }
                   }
               ],
               dragSelection: {
@@ -45,20 +52,63 @@ export default class Waveform extends React.Component{
         })
       ]
     });
-    wavesurfer.load('http://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3');
-    this.setState({wavesurfer})
+
+    // listeners
+    wavesurfer.on('finish',  () => {
+      this.setState({playing: false});
+    });
+    wavesurfer.on('region-in',(region) => {
+      this.setState({regionnote: region.data.note});
+    });
+    wavesurfer.on('region-click',(region) => {
+      this.setState({regionnote: region.data.note});
+    });
+    this.setState({wavesurfer});
   }
+
+  componentDidUpdate(prevProps) {
+    // Typical usage (don't forget to compare props):
+    if (this.props.audio !== prevProps.audio) {
+      console.log("Wave Player Updated")
+      if (this.props.audio === null){
+        this.state.wavesurfer.empty()
+      } else {
+        this.state.wavesurfer.loadBlob(this.props.audio);
+      }
+      this.setState({
+        playing: false
+      })
+    }
+  }
+
+
 
   render(){
     return(
-      <div>
-        <button
-          onClick={() => {
-              this.state.wavesurfer.playPause()
-          }}
-        />
+      <div className="WaveformContainer">
+        {this.state.regionnote}
         <div id="waveform"></div>
         <div id="wave-timeline"></div>
+        <div id ="waveform-button-container">
+          <button
+            onClick={() => {
+                this.state.wavesurfer.playPause();
+                this.setState({
+                  playing: !this.state.playing
+                });
+            }}>
+          {this.state.playing? 'Pause' : 'Play'}
+          </button>
+          <button
+            onClick={() => {
+                this.state.wavesurfer.stop();
+                this.setState({
+                  playing: false
+                });
+            }}>
+          Stop
+          </button>
+        </div>
       </div>
     )
   }
